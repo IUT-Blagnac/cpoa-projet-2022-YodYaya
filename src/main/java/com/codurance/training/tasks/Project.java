45a5898f396056a413a16d7e776fadc877a27198
lucas <lucas@Pavillon>
2022-01-04 12:09:04 +0100

    class project

--- src/main/java/com/codurance/training/tasks/Project.java
+++ src/main/java/com/codurance/training/tasks/Project.java
@@ -1,5 +1,65 @@
 package com.codurance.training.tasks;
 
-public class Project {
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class Project {
+	
+	private final String name;
+	private final ArrayList<Task> tasks;
+	
+	
+	
+	
+    public Project(String name) {
+        this.name = name;
+        this.tasks = new ArrayList<Task>();
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+
+    public Task getTask(long idTask) {
+        return this.tasks.get((int) idTask);
+    }
+
+/*
+    public List<Task> getTasksDueFor(LocalDate date) {
+        List<Task> tasksDue = new ArrayList<>();
+        for (Task task : this.tasks.values()) {
+            if (date.equals(task.getDeadline())) {
+                tasksDue.add(task);
+            }
+        }
+        return tasksDue;
+    }*/
+
+/*
+    public List<Task> getTasksWithDeadline() {
+        List<Task> tasksWithDeadline = new ArrayList<>();
+        for (Task task : this.tasks.values()) {
+            if (task.getDeadline() != null) {
+                tasksWithDeadline.add(task);
+            }
+        }
+        return tasksWithDeadline;
+    }
+*/
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+    
+
 
 }
+    
+	
+
