9aa839de3dacbcfeedce3d4dce4886f4ad75b2cb
YodYaya <yazid.yahia81@gmail.com>
2022-01-04 11:32:39 +0100

    Modification Class

--- src/main/java/com/codurance/training/tasks/command/Show.java
+++ src/main/java/com/codurance/training/tasks/command/Show.java
@@ -1,5 +1,26 @@
 package com.codurance.training.tasks.command;
 
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.codurance.training.tasks.Task;
+
 public class Show {
+	
+	 private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
+	 private final PrintWriter out = null;
+	 
+	@SuppressWarnings("unused")
+	private void show() {
+        for (Map.Entry<String, List<Task>> project : tasks.entrySet()) {
+            out.println(project.getKey());
+            for (Task task : project.getValue()) {
+                out.printf("    [%c] %d: %s%n", (task.isDone() ? 'x' : ' '), task.getId(), task.getDescription());
+            }
+            out.println();
+        }
+    }
 
 }
