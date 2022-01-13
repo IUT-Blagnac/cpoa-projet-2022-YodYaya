bd5eb94a2cca09b8491bb32ba3a89abb3a32438a
Zelium <80945987+ZeliumX@users.noreply.github.com>
2022-01-13 14:10:33 +0100

    Update Help.java
    class de comportement de la commande help

--- src/main/java/com/codurance/training/tasks/command/Help.java
+++ src/main/java/com/codurance/training/tasks/command/Help.java
@@ -1,28 +1,25 @@
 package com.codurance.training.tasks.command;
 
-import java.io.PrintWriter;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import com.codurance.training.tasks.TaskList;
 
-import com.codurance.training.tasks.objects.Task;
+public class Help  {
 
-public class Help {
 	
-	private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
-	 private final PrintWriter out = null;
+	public Help() {
+		
+	String chaine ="";	
+		
+	chaine += ""; 
+		
+	chaine += "Commands:";
+	chaine += "  show";
+	chaine += "  add project <project name>";
+	chaine += "  add task <project name> <task description>";
+	chaine += "  check <task ID>";
+	chaine += "  uncheck <task ID>";
+    
+	TaskList.sayConsole(chaine,true);
+    }
 	
-	 @SuppressWarnings("unused")
-	private void help() {
-	        out.println("Commands:");
-	        out.println("  show");
-	        out.println("  add project <project name>");
-	        out.println("  add task <project name> <task description>");
-	        out.println("  check <task ID>");
-	        out.println("  uncheck <task ID>");
-	        out.println();
-	    }
-
-
+	
 }
-
