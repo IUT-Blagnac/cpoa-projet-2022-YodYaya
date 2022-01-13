f4461133ece84ef700535502082e5b20755ab376
Zelium <80945987+ZeliumX@users.noreply.github.com>
2022-01-13 14:11:58 +0100

    Update Uncheck.java
    class de comportement de la commande uncheck

--- src/main/java/com/codurance/training/tasks/command/Uncheck.java
+++ src/main/java/com/codurance/training/tasks/command/Uncheck.java
@@ -1,6 +1,32 @@
 package com.codurance.training.tasks.command;
 
-public class Uncheck {
+import com.codurance.training.tasks.TaskList;
+import com.codurance.training.tasks.objects.Project;
+import com.codurance.training.tasks.objects.Task;
 
+public class Uncheck  {
+
+	public Uncheck(String idString) {
+		
+        
+		for(Project projet : TaskList.projetsList) {
+			
+			for (Task task : projet.getTaskList()) {
+				
+				if(task.getId() == Integer.parseInt(idString)) {
+					task.setDone(false);
+					return;
+					
+				}
+				
+			}
+		}
+		
+		
+		TaskList.sayConsole("Could not find a task with an ID of \""+idString+"\".",true);
+		
+		
+		
+    }
 	
 }
