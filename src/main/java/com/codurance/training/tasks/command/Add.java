c0142890cfe8304a976eb280f15dfd88b73c009b
Zelium <80945987+ZeliumX@users.noreply.github.com>
2022-01-13 14:08:50 +0100

    Update Add.java
    class de comportement de la commande Add

--- src/main/java/com/codurance/training/tasks/command/Add.java
+++ src/main/java/com/codurance/training/tasks/command/Add.java
@@ -1,52 +1,61 @@
 package com.codurance.training.tasks.command;
 
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
+import com.codurance.training.tasks.TaskList;
+import com.codurance.training.tasks.objects.Project;
 import com.codurance.training.tasks.objects.Task;
+
 
 public class Add {
 	
-	private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
-	 private final PrintWriter out = null;
-	 private long lastId = 0;
-	 
-	   @SuppressWarnings("unused")
-	private void add(String commandLine) {
-	        String[] subcommandRest = commandLine.split(" ", 2);
-	        String subcommand = subcommandRest[0];
-	        if (subcommand.equals("project")) {
-	            addProject(subcommandRest[1]);
-	        } else if (subcommand.equals("task")) {
-	            String[] projectTask = subcommandRest[1].split(" ", 2);
-	            addTask(projectTask[0], projectTask[1]);
-	        }
-	    }
-	   
+	
+    public Add(String commandLine) {
+        String[] subcommandRest = commandLine.split(" ", 2);
+        String subcommand = subcommandRest[0];
+        if (subcommand.equals("project")) {
+            addProject(subcommandRest[1]);
+        } else if (subcommand.equals("task")) {
+            String[] projectTask = subcommandRest[1].split(" ", 2);
+            addTask(projectTask[0], projectTask[1]);
+        }
+    }
 
-	    private void addProject(String name) {
-	        tasks.put(name, new ArrayList<Task>());
-	    }
-	    
-	   
+    private void addProject(String name) {
+        TaskList.projetsList.add(new Project(name));
+    }
 
-	    private void addTask(String project, String description) {
-	        List<Task> projectTasks = tasks.get(project);
-	        if (projectTasks == null) {
-	            out.printf("Could not find a project with the name \"%s\".", project);
-	            out.println();
-	            return;
-	        }
-	        projectTasks.add(new Task(nextId(), description, false));
-	    }
+    
+    
+    private void addTask(String project, String description) {
+    	
+    	int idProject = getProject(project);
+        if (idProject == -1) {
+            TaskList.sayConsole("Could not find a project with the name \""+project+"\".",false);
+            
+            return;
+        }
+        TaskList.projetsList.get(idProject).addTask(new Task(TaskList.lastIdTask,description,false));
+        
+        TaskList.lastIdTask++;
+    }
+    	
+    	
+
+
+	private int getProject(String projectName) {
+	
+	
+		for(int i=0;i<TaskList.projetsList.size();i++) {
+		
+			if(TaskList.projetsList.get(i).getName().equals(projectName)) return i ;
+		
+		}
+	
+		return -1;
+	
+}
 	    
 	    
-	    private long nextId() {
-	        return ++lastId;
-	    }
+
 
 }
-
