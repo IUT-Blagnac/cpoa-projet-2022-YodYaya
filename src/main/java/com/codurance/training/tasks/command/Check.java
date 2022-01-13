698f67410f98ae6f34907755282e9fb4241c02f9
Zelium <80945987+ZeliumX@users.noreply.github.com>
2022-01-13 14:09:27 +0100

    Update Check.java
    class de comportement de la commande check

--- src/main/java/com/codurance/training/tasks/command/Check.java
+++ src/main/java/com/codurance/training/tasks/command/Check.java
@@ -1,6 +1,36 @@
 package com.codurance.training.tasks.command;
 
+import com.codurance.training.tasks.TaskList;
+import com.codurance.training.tasks.objects.Project;
+import com.codurance.training.tasks.objects.Task;
+
 public class Check {
 
-}
 
+	public Check(String idString) {
+		
+        
+		for(Project projet : TaskList.projetsList) {
+			
+			for (Task task : projet.getTaskList()) {
+				
+				if(task.getId() == Integer.parseInt(idString)) {
+					task.setDone(true);
+					return;
+					
+				}
+				
+			}
+		}
+		
+		
+		TaskList.sayConsole("Could not find a task with an ID of \""+idString+"\".",true);
+		
+		
+		
+    }
+	
+	
+	
+	
+}
